e93881d3e51f73eb8321de82568da993d16b41e3
ABINAYA S <165631299+AbinayaSekar21@users.noreply.github.com>
2025-03-22 12:36:42 +0530

    ListOfComments.java

--- project/src/main/java/com/example/project/ListOfComments.java
+++ project/src/main/java/com/example/project/ListOfComments.java
@@ -2,25 +2,25 @@ package com.example.project;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ListOfComments {
+
+    @JsonProperty("comments")
     private List<Comment> comments;
 
-    // Constructor
     public ListOfComments(List<Comment> comments) {
         this.comments = comments;
     }
 
-    // Getter
     public List<Comment> getComments() {
         return comments;
     }
 
-    // Setter
     public void setComments(List<Comment> comments) {
         this.comments = comments;
     }
 
-    // ToString Method
     @Override
     public String toString() {
         return "ListOfComments{" +
